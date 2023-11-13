047140f7ecfdc65cad7355a41275b074287380af
Yeongdong <jyd37855@gmail.com>
2023-11-13 23:43:55 +0900

    style: 코드 단순화

--- src/main/java/christmas/domain/Customer.java
+++ src/main/java/christmas/domain/Customer.java
@@ -20,7 +20,7 @@ public class Customer {
 
     public int getTotalPriceBeforeDiscount() {
         return getOrderedMenusList().stream()
-                .mapToInt(orderedMenu -> orderedMenu.getPrice())
+                .mapToInt(OrderedMenu::getPrice)
                 .sum();
     }
 
