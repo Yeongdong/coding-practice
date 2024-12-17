07e153683a170c55a46b7cf6f7b106167b6a42b5
Yeongdong <jyd37855@gmail.com>
2024-12-17 19:08:57 +0900

    chore: add cors configuration

---
+++ src/main/java/com/trading/stock_trading/global/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.trading.stock_trading.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://3.36.133.239")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
